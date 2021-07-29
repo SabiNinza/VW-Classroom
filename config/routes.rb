7cfa1885201cfdb3b8108f8bc6f6f8012b822c45
nabhardwaj <bhardwaj.ashwani92@gmail.com>
2021-07-29 12:06:42 +0530

    Route update

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@
 
 Rails.application.routes.draw do
   get '/health_check', to: 'health_check#all'
-
+  get '/start_class', to: 'sessions#autoLogin', as: :autoLogin
   # Error routes.
   match '/401', to: 'errors#unauthorized', via: :all, as: :unauthorized
   match '/404', to: 'errors#not_found', via: :all, as: :not_found
